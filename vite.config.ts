8aba55359e6d320fd0b4f1eb123fecd26aed7285
Aldo Frota <aldofrotadev@gmail.com>
2025-06-07 10:35:14 -0300

    feat: adicionado suporte ao Tailwind CSS e reorganização de arquivos

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,5 @@ export default defineConfig({
     port: 3000,
     host: true,
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
