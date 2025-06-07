1d786304c50ad4351c771b5f3ceaee5c322963d0
Aldo Frota <aldofrotadev@gmail.com>
2025-06-07 11:38:37 -0300

    feat: atualização do título do template, renomeação do projeto e adição do react-icons; melhorias na página do Dashboard

--- src/features/dashboard/pages/Dashboard.tsx
+++ src/features/dashboard/pages/Dashboard.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { ThemeToggle } from "@/components";
+import { LuLayoutDashboard } from "react-icons/lu";
 
 const Dashboard: React.FC = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="bg-gray-200 p-2 rounded-md flex items-center justify-center cursor-pointer">
+        <ThemeToggle />
+      </div>
+      <h1 className="text-2xl font-bold flex items-center gap-2">
+        <LuLayoutDashboard />
+        <span>Dashboard</span>
+      </h1>
     </div>
   );
 };
