a83c3ebdb2aa9f1a64a1293774268084c60800ef
Aldo Frota <aldofrotadev@gmail.com>
2025-06-07 11:02:27 -0300

    feat: adicionado react-router-dom e configurado roteamento no aplicativo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,17 @@
-import { DatePicker } from "antd";
-import { useState } from "react";
-import { ThemeToggle } from "@/components/ThemeToggle";
+import React from "react";
+import { ConfigProvider } from "antd";
+import { themeConfig } from "@/styles/theme";
+import ptBR from "antd/es/locale/pt_BR";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
-function App() {
-  return <></>;
-}
+const App: React.FC = () => {
+  return (
+    <ConfigProvider theme={themeConfig} locale={ptBR}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ConfigProvider>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "@/components/App";
+import { BrowserRouter } from "react-router-dom";
 import "@/styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
